ad3da7173335d6a02f11364359a704b636eb1427
gedesurya125 <gedesurya125@gmail.com>
2021-10-29 13:55:57 +0800

    setup environtment conditional

--- src/redux/APIs/setup.js
+++ src/redux/APIs/setup.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const baseURL = "http://localhost:5000";
+export const baseURL = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://aiatestserver.herokuapp.com";
+// console.log('ENVIRONTMENT NYA BROH', process.env.NODE_ENV);
 
 export const baseAPI = axios.create({
   baseURL
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,8 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware]
-const composeEnhancer = composeWithDevTools || compose;
+const composeEnhancer = process.env.NODE_ENV === 'development' ? composeWithDevTools : compose;
+// console.log('ENVIRONTMENT ON STORE', process.env.NODE_ENV);
 
 const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)));
 
